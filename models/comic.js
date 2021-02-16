a3860753e2621303eea29ff4408aed90a337643d
Steve Emerson <sbemerson@outlook.com>
2021-02-16 18:16:51 -0500

    Added first endpoint to shelfcontroller and built comic model

---
+++ models/comic.js
@@ -0,0 +1,75 @@
+const sequelize = require("../db");
+
+module.exports = (sequelize, DataTypes) => {
+  const Comic = sequelize.define('comic', {
+    issue_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    issue_name: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    issue_number: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    cover_date: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    volume_name: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    volume_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    story_arcs: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: true
+    },
+    publisher: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    teams: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: true
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    characters: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: true
+    },
+    original_image_url: {
+      type: DataTypes.STRING(1000),
+      allowNull: true
+    },
+    thumb_image_url: {
+      type: DataTypes.STRING(1000),
+      allowNull: true
+    },
+    small_image_url: {
+      type: DataTypes.STRING(1000),
+      allowNull: true
+    },
+    api_detail_url: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    owner: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    status: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+  })
+  return Comic;
+};
